46111efe347114a8a13b0ab2d2b684a44e28430b
Evan Harris <echarris@smcm.edu>
2017-08-07 15:59:28 -0400

    Updates package.json'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
