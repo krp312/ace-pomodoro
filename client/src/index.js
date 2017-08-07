e285b00b98384c02a3626cb9381bf82dd1f8840b
Evan Harris <echarris@smcm.edu>
2017-08-07 14:33:53 -0400

    Initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 
-// import App from './App';
+import App from './App';
 import './index.css';
-import CheeseList from './components/cheese-list';
+// import CheeseList from './components/cheese-list';
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <App />*/}
-    <CheeseList/>
+     <App />
+    {/* <CheeseList/> */}
   </Provider>,
   document.getElementById('root')
 );
