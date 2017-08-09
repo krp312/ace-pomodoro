703a437e74c52ba664e35bf81bb451eaa70d9738
krp312 <krp312@me.com>
2017-08-09 02:09:50 -0400

    finish user creation endpoint, comment out unfinished endpoints

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -8,24 +8,24 @@ const { authenticator } = require('../auth');
 router.use(bodyParser.json());
 
 // get sessions by user id (user id from req.user)
-router.get('/', authenticator, (req, res) => {
-  return req.app.locals.knex
-    .select()
-    .from('sessions')
-    .then(result => {
-      return res.status(200).json(result);
-    });
-});
+// router.get('/', authenticator, (req, res) => {
+//   return req.app.locals.knex
+//     .select()
+//     .from('sessions')
+//     .then(result => {
+//       return res.status(200).json(result);
+//     });
+// });
 
-// Query single session from DB
-router.get('/:id', (req, res) => {
-  // What occurs in here depends on postgres data structure
-  return res.status(200).json({tagName: 'Postgres Study'});
-});
+// // Query single session from DB
+// router.get('/:id', (req, res) => {
+//   // What occurs in here depends on postgres data structure
+//   return res.status(200).json({tagName: 'Postgres Study'});
+// });
 
 // Create a new session in DB 
-router.post('/', (req, res) => {
-  const requiredFields = ['name', 'workDuration', 'breakDuration'];
+router.post('/', authenticator, (req, res) => {
+  const requiredFields = ['name', 'work_duration', 'break_duration'];
   const missingIndex = requiredFields.findIndex(field => !req.body[field]);
   if (missingIndex !== -1) {
     return res.status(400).json({
@@ -33,17 +33,18 @@ router.post('/', (req, res) => {
     });
   }
 
-  // update for req.user
+  let { name, work_duration, break_duration } = req.body;
+
   return req.app.locals.knex
     .insert({ 
-      name: req.body.name, 
-      work_duration: req.body.workDuration, 
-      break_duration: req.body.breakDuration, 
-      user_id: 2
+      name,
+      work_duration,
+      break_duration,
+      user_id: req.user.id
     })
     .into('sessions')
     .returning([
-      'id', 
+      'id',
       'modified', 
       'name', 
       'work_duration', 
@@ -55,17 +56,20 @@ router.post('/', (req, res) => {
     ])
     .then(result => {
       return res.status(201).json(result);
+    })
+    .catch(error => {
+      return res.status(500).send(error);
     });
 });
 
-// Alter session data - should require auth
-router.put('/:id', (req, res) => {
-  return res.status(204).json({tagName: 'Super Fun Postgres Study'});
-});
+// // Alter session data - should require auth
+// router.put('/:id', (req, res) => {
+//   return res.status(204).json({tagName: 'Super Fun Postgres Study'});
+// });
 
-// Delete session data - require auth
-router.delete('/:id', (req, res) => {
-  return res.status(204).end();
-});
+// // Delete session data - require auth
+// router.delete('/:id', (req, res) => {
+//   return res.status(204).end();
+// });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,71 +6,66 @@ const bcrypt = require('bcryptjs');
 router.use(bodyParser.json());
 
 // Get all users from DB
-router.get('/', (req, res) => {
-  // http://expressjs.com/en/4x/api.html#app.locals
-  // app.locals is an object that has local variables
-  // throughout the life of the app
-  // access the object via `req.app.whatever-property`
-  return req.app.locals.knex.select().from('users')
-    .then(result => {
-      res.status(200).json(result);
-    });
-});
+// router.get('/', (req, res) => {
+//   // http://expressjs.com/en/4x/api.html#app.locals
+//   // app.locals is an object that has local variables
+//   // throughout the life of the app
+//   // access the object via `req.app.whatever-property`
+//   return req.app.locals.knex.select().from('users').then(result => {
+//     res.status(200).json(result);
+//   });
+// });
 
 // Query single user from DB
-router.get('/:id', (req, res) => {
-  // What occurs in here depends on postgres data structure
-  return res.status(200).json({name: 'bobby'});
-});
+// router.get('/:id', (req, res) => {
+//   // What occurs in here depends on postgres data structure
+//   return res.status(200).json({ name: 'bobby' });
+// });
 
-// Create a new user in DB 
+// Create a new user in DB
 router.post('/', (req, res) => {
+  const requiredFields = ['username', 'password'];
+  const missingIndex = requiredFields.findIndex(field => !req.body[field]);
+  if (missingIndex !== -1) {
+    return res.status(400).json({
+      message: `Missing field: ${requiredFields[missingIndex]}`
+    });
+  }
+
+  let { username, password } = req.body;
+  username = username.trim();
+  password = password.trim();
+
   let hashedPassword;
 
-  bcrypt.hash(req.body.password, 10)
+  bcrypt.hash(password, 10)
     .then(result => {
       hashedPassword = result;
 
       return req.app.locals.knex
         .insert({
-          username: req.body.username,
+          username,
           password: hashedPassword
         })
         .into('users')
-        .returning([
-          'username',
-          'password'
-        ]);
+        .returning(['username', 'password']);
     })
-    .then(result => {
-      res.status(201).send(result);
+    .then(() => {
+      res.status(201).json({ message: 'user create success' });
     })
     .catch(error => {
       res.status(500).json({ message: 'error creating user' });
     });
-
-  // const requiredFields = ['username', 'password', 'email'];
-  // const missingIndex = requiredFields.findIndex(field => !req.body[field]);
-  // if (missingIndex !== -1) {
-  //   return res.status(400).json({
-  //     message: `Missing field: ${requiredFields[missingIndex]}`
-  //   });
-  // }
-
-  // let {username, password, email} = req.body;
-  // username = username.trim();
-  // password = password.trim();
-  // return res.status(201).json(req.body);
 });
 
 // Alter user data - should require auth
-router.put('/:id', (req, res) => {
-  return res.status(204).json({name: 'bobbed'});
-});
+// router.put('/:id', (req, res) => {
+//   return res.status(204).json({ name: 'bobbed' });
+// });
 
 // Delete user data - require auth
-router.delete('/:id', (req, res) => {
-  return res.status(204).end();
-});
+// router.delete('/:id', (req, res) => {
+//   return res.status(204).end();
+// });
 
 module.exports = router;
