fa820efa819625792e6ac8fb94b9a6d7f4fbd6f9
Evan Harris <echarris@smcm.edu>
2017-08-08 23:37:34 -0400

    Adds Break button timer

--- client/src/components/break-timer.js
+++ client/src/components/break-timer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './styles/break-timer.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./styles/break-timer.css";
 // import { fetchCheeses } from '../actions/cheese'
 
 export class BreakTimer extends React.Component {
@@ -9,19 +9,27 @@ export class BreakTimer extends React.Component {
   // }
 
   render() {
-
+    let { secondsRemaining, minutesRemaining } = this.props;
     return (
       <div className="break-timer">
         <p>this is the main pomo timer component for the break period</p>
+        <div className="clock" role="timer">
+          <span className="minutes">
+            {minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining}
+          </span>
+          <span className="colon">:</span>
+          <span className="seconds">
+            {secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining}
+          </span>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-// const mapStateToProps = (state) => (
-//   {
-//     minutesRemaining: state.minutesRemaining,
-//     secondsRemaining: state.secondsRemaining
-// })
+const mapStateToProps = state => ({
+  minutesRemaining: state.breakMinutesRemaining,
+  secondsRemaining: state.breakSecondsRemaining
+});
 
-export default connect()(BreakTimer);
+export default connect(mapStateToProps)(BreakTimer);
