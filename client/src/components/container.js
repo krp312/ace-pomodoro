aa3424da734f61b488d226fde7dac67caa68c244
Evan Harris <echarris@smcm.edu>
2017-08-10 00:03:57 -0400

    Prevents user from restarting timer if it is still running

--- client/src/components/container.js
+++ client/src/components/container.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import LogIn from './login';
-import CreateUser from './create-user';
-import UserData from './user-data';
-import SetPomo from './set-pomo';
-import WorkTimer from './work-timer';
-import BreakTimer from './break-timer';
-import PomoInfo from './pomo-info';
-import Header from './header';
+import LogIn from "./login";
+import CreateUser from "./create-user";
+import UserData from "./user-data";
+import SetPomo from "./set-pomo";
+import WorkTimer from "./work-timer";
+import BreakTimer from "./break-timer";
+import PomoInfo from "./pomo-info";
+import Header from "./header";
 
-import './styles/container.css';
+import "./styles/container.css";
 
-export default function App(props) {
-    const sessionActive = false;
-    // Create state variable to keep track of when sessionMinutesRemaining and secondsRemaining === 0
-    // Make sure that both create user button and login user button LEAD to WorkTimer component while
-        // The session is still active
+export class App extends React.Component {
+  // const sessionActive = false;
+  // Create state variable to keep track of when sessionMinutesRemaining and secondsRemaining === 0
+  // Make sure that both create user button and login user button LEAD to WorkTimer component while
+  // The session is still active
+  render() {
     return (
-        <Router>
-            <div className="app">
-                <Header/>
-                <main>
-                    <Route exact path="/" component={LogIn} />
-                    <Route exact path="/create-user" component={CreateUser} />
-                    <Route exact path="/user-data" component={UserData} />
-                    <Route exact path="/work-timer" component={WorkTimer}/>
-                    <Route exact path="/break-timer" component={BreakTimer}/>
-                    <Route exact path="/pomo-info" component={PomoInfo} />
-                    <Route exact path="/set-pomo" render={() => sessionActive ? <UserData /> : <SetPomo />}/>
-                </main>
-            </div>
-        </Router>
+      <Router>
+        <div className="app">
+          <Header />
+          <main>
+            <Route exact path="/" component={LogIn} />
+            <Route exact path="/create-user" component={CreateUser} />
+            <Route exact path="/user-data" component={UserData} />
+            <Route exact path="/work-timer" component={WorkTimer} />
+            <Route exact path="/break-timer" component={BreakTimer} />
+            <Route exact path="/pomo-info" component={PomoInfo} />
+            <Route exact path="/set-pomo" component={SetPomo}/>
+          </main>
+        </div>
+      </Router>
     );
+  }
 }
 
+export default connect()(App);
