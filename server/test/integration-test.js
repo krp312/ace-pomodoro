5922e44b57bdb77361aabc9c20c82d7233fd701e
krp312 <krp312@me.com>
2017-08-10 03:21:57 -0400

    comment out integration-test.js

---
+++ server/test/integration-test.js
@@ -0,0 +1,41 @@
+// 'use strict';
+
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const faker = require('faker');
+
+// const should = chai.should();
+
+// const {app, runServer, closeServer} = require('../index');
+
+// chai.use(chaiHttp);
+
+// // How do we tear down db using Knex?
+
+// function tearDownDb() {
+//   console.warn('Deleting database');
+// }
+
+// describe('Sessions API resource', function() {
+//   it('should pass', function() {
+//     true.should.equal(true);
+//   });
+//   // it('Should get all sessions', function() {
+//   //   let res;
+//   //   return chai.request(app)
+//   //     .get('/api/session')
+//   //     .then(function(_res){
+//   //       res=_res;
+//   //       res.should.have.status(200);
+//   //       res.should.be.json;
+//   //     });
+//   });
+// });
+
+// describe('Users API resource', function() {
+//   it('should pass', function() {
+//     true.should.equal(true);
+//   });
+
+  
+// });
