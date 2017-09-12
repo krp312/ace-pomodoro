04599bfa5346d96e7411002d096028e15591ce5d
krp312 <krp312@me.com>
2017-09-12 00:52:17 -0400

    Reboot app

--- server/config.js
+++ server/config.js
@@ -1,10 +1,13 @@
-const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://localhost/ace-pomodoro';
+const DATABASE_URL =
+    process.env.DATABASE_URL ||
+    global.DATABASE_URL ||
+    'postgresql://localhost/ace-pomodoro';
 
 exports.DATABASE = {
-  client: 'pg',
-  connection: DATABASE_URL,
-  pool: { min: 0, max: 3 },
-  debug: true
+    client: 'pg',
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 3 },
+    debug: true
 };
 
 exports.PORT = process.env.PORT || 3001;
