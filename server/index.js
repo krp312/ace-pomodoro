756bb2e76fa43dcc73ffbbd2f286bd89eb278dba
krp312 <krp312@me.com>
2017-09-12 00:13:44 -0400

    Dunno

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const userRouter = require('./routes/users');
 
 const { DATABASE, PORT } = require('./config');
 
-const { passportMiddleware } = require('./auth')
+const { passportMiddleware } = require('./auth');
 
 app.use(passportMiddleware);
 
@@ -32,58 +32,62 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Allows CORS
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
 });
 
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-  const index = path.resolve(__dirname, '../client/build', 'index.html');
-  res.sendFile(index);
+    const index = path.resolve(__dirname, '../client/build', 'index.html');
+    res.sendFile(index);
 });
 
 let server;
 let knex;
 function runServer(database = DATABASE, port = PORT) {
-  return new Promise((resolve, reject) => {
-    try {
-      // http://expressjs.com/en/4x/api.html#app.locals
-      // app.locals is an object that has local variables
-      // throughout the life of the app
-      // access the object via `req.app.whatever-property`
-      app.locals.knex = require('knex')(database);
-      server = app.listen(port, () => {
-        console.info(`App listening on port ${server.address().port}`);
-        resolve();
-      });
-    }
-    catch (err) {
-      console.error(`Can't start server: ${err}`);
-      reject(err);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        try {
+            // http://expressjs.com/en/4x/api.html#app.locals
+            // app.locals is an object that has local variables
+            // throughout the life of the app
+            // access the object via `req.app.whatever-property`
+            app.locals.knex = require('knex')(database);
+            server = app.listen(port, () => {
+                console.info(`App listening on port ${server.address().port}`);
+                resolve();
+            });
+        } catch (err) {
+            console.error(`Can't start server: ${err}`);
+            reject(err);
+        }
+    });
 }
 
 function closeServer() {
-  return app.locals.knex.destroy().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing servers');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
+    return app.locals.knex.destroy().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing servers');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
-  });
 }
 
 if (require.main === module) {
-  runServer();
+    runServer();
 }
 
 module.exports = {
-  app, runServer, closeServer
+    app,
+    runServer,
+    closeServer
 };
