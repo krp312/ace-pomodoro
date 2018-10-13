3fefa5af650b402610b9794e09dd2127fe6ee80b
Kristoffer Panahon <krp312@me.com>
2018-10-13 18:23:12 -0400

    Remove login indicator (currently broken)

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -12,21 +12,6 @@ export class Header extends React.Component {
     this.props.dispatch(logoutUser());
   }
   
-  renderUser() {
-    if (this.props.loggedIn === true) {
-      return (
-        <div>
-          <span>
-          {this.props.username}
-          </span>
-          <button className="logout-button" onClick={e => this.logoutUser(e)}>Logout</button>
-        </div>
-      );
-    } else {
-      return <span>Not logged in</span>;
-    }
-  }
-  // if user is logged in, clicking Ace Pomodoro doesnt direct to login page
   conditionalMainPage() {
     if (this.props.loggedIn === false) {
       return <Link to="/">Ace Pomodoro</Link>
@@ -38,11 +23,6 @@ export class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <span className="users-span">
-          <Link to="/user-data">
-            {this.renderUser()}
-          </Link>
-        </span>
         <h1>
           {this.conditionalMainPage()}
         </h1>
