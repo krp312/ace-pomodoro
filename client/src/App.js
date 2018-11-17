efc97d7d931171a29c653948d69830d7e2d3c997
Kristoffer Panahon <krp312@me.com>
2018-11-17 00:33:58 -0500

    Move Container into App (application is compiling)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import Container from './components/container';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LogIn from './components/login';
+import CreateUser from './components/create-user';
+import UserData from './components/user-data';
+import SetSessionTimes from './components/set-session-times';
+import WorkTimer from './components/work-timer';
+import BreakTimer from './components/break-timer';
+import PomoInfo from './components/pomo-info';
+import Header from './components/header';
+import './components/styles/container.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Container/>
-      </div>
+      <Router>
+        <div>
+          <Header/>
+          <main>
+            <Route exact path="/" component={LogIn} /> 
+            <Route exact path="/create-user" component={CreateUser} />
+            <Route exact path="/pomo-info" component={PomoInfo} />
+            <Route exact path="/user-data" component={UserData} />
+            <Route exact path="/work-timer" component={WorkTimer}/>
+            <Route exact path="/break-timer" component={BreakTimer}/>
+            <Route exact path="/set-session-times" component={SetSessionTimes}/>
+            <Route exact path="/set-session-times/:sessionId" component={SetSessionTimes}/>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
