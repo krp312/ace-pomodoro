9a36829f4e699615139ffd7c0d7a0a159cd5af3f
Kristoffer Panahon <krp312@me.com>
2018-11-18 15:05:47 -0500

    Add Timer component tests

--- client/src/components/timer.test.js
+++ client/src/components/timer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { Timer } from './timer';
 
 describe('<Timer />', () => {
@@ -10,21 +10,32 @@ describe('<Timer />', () => {
   it('dispatches countDownWorkTime from countdownTimer', () => {
     const dispatch = jest.fn();
     shallow(<Timer dispatch={dispatch} initialWorkMinutes={60} timerType={'work'} />);
-    expect(dispatch).toHaveBeenCalledWith({"type": "COUNT_DOWN_WORK_TIME", "workTimeRemaining": "00:01:00"});
+    expect(dispatch).
+      toHaveBeenCalledWith({"type": "COUNT_DOWN_WORK_TIME", "workTimeRemaining": "00:01:00"});
   });
 
   it('dispatches countDownBreakTime from countdownTimer', () => {
-    // const dispatch = jest.fn();
-    // shallow(<Timer 
-    //   dispatch={dispatch} 
-    //   initialWorkMinutes={1}
-    //   initialBreakMinutes={60} 
-    //   workTimeRemaining={'00:00:00'}
-    //   breakTimeRemaining={null} 
-    //   timerType={'break'} />);
-    // expect(dispatch).toHaveBeenCalledWith({"type": "COUNT_DOWN_BREAK_TIME", "breakTimeRemaining": "00:01:00"});
+    const dispatch = jest.fn();
+    const wrapper = shallow(<Timer 
+      dispatch={dispatch} 
+      initialBreakMinutes={60} 
+      breakTimeRemaining={null} 
+      timerType={'break'} 
+    />);
+    wrapper.instance().componentDidUpdate();
+    expect(dispatch).
+      toHaveBeenCalledWith({"type": "COUNT_DOWN_BREAK_TIME", "breakTimeRemaining": "00:01:00"});
   });
 
-  it('dispatches setTimerType from componentDidUpdate()', () => {
+  it('dispatches setTimerType from componentDidUpdate', () => {
+    const dispatch = jest.fn();
+    const wrapper = shallow(
+      <Timer 
+        dispatch={dispatch} 
+        workTimeRemaining={'00:00:00'} 
+        timerType={'work'} 
+      />);
+    wrapper.instance().componentDidUpdate();
+    expect(dispatch).toHaveBeenCalledWith({"timerType": "break", "type": "SET_TIMER_TYPE"});
   });
 });
