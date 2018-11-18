553cf5a2967078bca3819f5fbb90200cf4863b1b
Kristoffer Panahon <krp312@me.com>
2018-11-18 04:45:23 -0500

    Add quick server test for CI

---
+++ server/test/test-true.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('A passing test just for now', function() {
+    it('is something I am trying to get CI to work', function() {
+      expect(true).to.be.true;
+    });
+  }
+);
