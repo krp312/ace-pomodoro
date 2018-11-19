22d006416cba3a24368b43828aaf59751c86ff06
Kristoffer Panahon <krp312@me.com>
2018-11-18 19:51:38 -0500

    Now about that linting life

--- client/src/components/start-session.js
+++ client/src/components/start-session.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setSessionTimes, setTimerType, setSessionName } from '../actions/index';
+import {
+  setSessionTimes,
+  setTimerType,
+  setSessionName
+} from '../actions/index';
 import './styles/start-session.css';
 
 export class StartSession extends React.Component {
   submitSession() {
-    this.props.dispatch(setSessionTimes(
-      this.initialWorkMinutes.value,
-      this.initialBreakMinutes.value
-    ));
+    this.props.dispatch(
+      setSessionTimes(
+        this.initialWorkMinutes.value,
+        this.initialBreakMinutes.value
+      )
+    );
     this.props.dispatch(setSessionName(this.sessionName.value));
     this.props.dispatch(setTimerType('work'));
     this.props.history.push('/timer');
@@ -16,37 +22,48 @@ export class StartSession extends React.Component {
 
   render() {
     return (
-      <form className="submit-sessions-form" onSubmit={() => this.submitSession()}>
-        <label htmlFor="initial-work-minutes">Work (minutes)</label>
-        <input
-          aria-label="initial work minutes"
-          type="text"
-          placeholder="25"
-          id="initial-work-minutes"
-          ref={input => this.initialWorkMinutes = input}
-          required
-        />
-        <label htmlFor="initial-break-minutes">Break (minutes)</label>
-        <input
-          aria-label="initial break minutes"
-          type="text"
-          placeholder="5"
-          id="initial-break-minutes"
-          ref={input => this.initialBreakMinutes = input}
-          required
-        />
-        <label htmlFor="session-name">Session Name</label>
-        <input
-          aria-label="session name"
-          type="text"
-          placeholder="Study React"
-          id="session-name"
-          ref={input => this.sessionName = input}
-          required
-        />
-        <button type="submit" className="start-session-button">Start</button>
+      <form
+        className="submit-sessions-form"
+        onSubmit={() => this.submitSession()}
+      >
+        <label htmlFor="initial-work-minutes">
+          Work (minutes)
+          <input
+            aria-label="initial work minutes"
+            type="text"
+            placeholder="25"
+            id="initial-work-minutes"
+            ref={(input) => { this.initialWorkMinutes = input; }}
+            required
+          />
+        </label>
+        <label htmlFor="initial-break-minutes">
+          Break (minutes)
+          <input
+            aria-label="initial break minutes"
+            type="text"
+            placeholder="5"
+            id="initial-break-minutes"
+            ref={(input) => { this.initialBreakMinutes = input; }}
+            required
+          />
+        </label>
+        <label htmlFor="session-name">
+          Session Name
+          <input
+            aria-label="session name"
+            type="text"
+            placeholder="Study React"
+            id="session-name"
+            ref={(input) => { this.sessionName = input; }}
+            required
+          />
+        </label>
+        <button type="submit" className="start-session-button">
+          Start
+        </button>
       </form>
-    )
+    );
   }
 }
 
